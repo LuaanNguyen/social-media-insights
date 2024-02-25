f8663abc3436825a8f9a86244fa1c9631106c923
ltnguy359 <153343039+ltnguy359@users.noreply.github.com>
2024-02-24 23:56:11 -0700

    Add more functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 
 function App() {
   return (
-    <>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/users">
-        <Users />
-      </Route>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/users" element={<Users />}></Route>
+    </Routes>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,175 @@
+import data from "../data.json";
+
+//filterCountru returns the users from different countries
+function filterCountry() {
+  let countries = {};
+  for (const user of data) {
+    if (user.user_profile.country in countries) {
+      countries[user.user_profile.country].push(user.user_profile);
+    } else {
+      countries[user.user_profile.country] = [user.user_profile];
+    }
+  }
+  console.log(countries);
+  return countries;
+}
+
+//filterTiktokFollowers returns the users from ranges of likes
+function filterTiktokFollowers(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    if (
+      user.user_profile.tiktok_followers >= lowerRange &&
+      user.user_profile.tiktok_followers <= upperRange
+    ) {
+      users.push(user.user_profile);
+    }
+  }
+  console.log(users);
+  return users;
+}
+
+//filterTikTokEmail returns the users who have tiktok email (useless)
+function filterTikTokEmail() {
+  let users = [];
+
+  for (const user of data) {
+    if (user.user_profile.tiktok_email === true) {
+      users.push(user.user_profile);
+    }
+  }
+  console.log(users);
+  return users;
+}
+
+//filterByPostCount returns the users who have
+function filterByPostCount(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    if (
+      user.posts_info.length >= lowerRange &&
+      user.posts_info.length <= upperRange
+    ) {
+      users.push(user.user_profile);
+    }
+  }
+  console.log(users);
+  return users;
+}
+
+function filterByTotalLikes(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalLike = 0;
+    for (const post of user.posts_info) {
+      totalLike += post.likes;
+    }
+    if (totalLike >= lowerRange && totalLike <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
+function filterByTotalViews(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalViews = 0;
+    for (const post of user.posts_info) {
+      totalViews += post.views;
+    }
+    if (totalViews >= lowerRange && totalViews <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
+function filterByTotalShares(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalShares = 0;
+    for (const post of user.posts_info) {
+      totalShares += post.shares;
+    }
+    if (totalShares >= lowerRange && totalShares <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
+function filterByTotalShares_WhatsApp(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalShares = 0;
+    for (const post of user.posts_info) {
+      totalShares += post.shares_whatsapp;
+    }
+    if (totalShares >= lowerRange && totalShares <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
+function filterByTotalComments(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalComments = 0;
+    for (const post of user.posts_info) {
+      totalComments += post.comments;
+    }
+    if (totalComments >= lowerRange && totalComments <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
+function filterByTotalSaved(lowerRange, upperRange) {
+  let users = [];
+
+  for (const user of data) {
+    let totalSaved = 0;
+    for (const post of user.posts_info) {
+      totalSaved += post.saved;
+    }
+
+    if (totalSaved >= lowerRange && totalSaved <= upperRange) {
+      users.push(user.user_profile);
+    }
+  }
+
+  console.log(users);
+  return users;
+}
+
 function Home() {
+  // filterCountry();
+  // filterTiktokFollowers(400, 10000);
+  // filterTikTokEmail();
+  // filterByPostCount(5, 10);
+  // filterByTotalLikes(1000, 3000);
+  // filterByTotalViews(20000, 22000);
+
   return <></>;
 }
 
