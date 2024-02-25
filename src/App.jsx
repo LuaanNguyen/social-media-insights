1a5fcd84e8e382f543b23412e432846fbf663688
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2024-02-24 21:31:49 -0700

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
